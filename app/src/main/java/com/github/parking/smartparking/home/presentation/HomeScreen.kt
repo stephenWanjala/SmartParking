985f0a559127db2e09ad8af10b9c27f11c6b5798
wanjala stephen <stephenwanjala145@gmail.com>
2024-04-03 20:09:43 +0300

    Provider Card

--- app/src/main/java/com/github/parking/smartparking/home/presentation/HomeScreen.kt
+++ app/src/main/java/com/github/parking/smartparking/home/presentation/HomeScreen.kt
@@ -2,17 +2,15 @@ package com.github.parking.smartparking.home.presentation
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.wrapContentHeight
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.lazy.staggeredgrid.LazyVerticalStaggeredGrid
+import androidx.compose.foundation.lazy.staggeredgrid.StaggeredGridCells
+import androidx.compose.foundation.lazy.staggeredgrid.items
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
@@ -51,9 +49,14 @@ fun HomeScreen() {
             },
 
             ) { paddingValues ->
-            LazyColumn(modifier = Modifier.padding(paddingValues)) {
+            LazyVerticalStaggeredGrid(
+                modifier = Modifier.padding(paddingValues),
+                columns = StaggeredGridCells.Adaptive(200.dp)
+            ) {
                 items(ParkingProvider.providers) { provider ->
-                    ParkingProviderCard(provider = provider)
+                    ParkingProviderCard(provider = provider) {
+
+                    }
                 }
 
             }
@@ -93,71 +96,65 @@ fun SmartParkingApBar(
 
 
 @Composable
-fun ParkingProviderCard(modifier: Modifier = Modifier, provider: ParkingProvider) {
+fun ParkingProviderCard(
+    modifier: Modifier = Modifier,
+    provider: ParkingProvider,
+    onclick: (ParkingProvider) -> Unit
+) {
     Card(
         modifier = modifier
             .fillMaxWidth()
             .padding(16.dp),
         shape = RoundedCornerShape(16.dp),
-        elevation = CardDefaults.cardElevation(defaultElevation = 8.dp)
+        elevation = CardDefaults.cardElevation(defaultElevation = 8.dp),
+        onClick = { onclick(provider) }
     ) {
         Column(
             modifier = Modifier
                 .fillMaxWidth()
                 .padding(16.dp)
         ) {
+            Image(
+                painter = painterResource(id = provider.imageUrl),
+                contentDescription = null,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(100.dp)
+                    .clip(RoundedCornerShape(16.dp)),
+                contentScale = androidx.compose.ui.layout.ContentScale.Crop
+            )
+
+            Spacer(modifier = Modifier.height(16.dp))
+
             Text(
                 text = provider.name,
                 style = MaterialTheme.typography.headlineSmall,
                 textAlign = TextAlign.Center
             )
 
+            Spacer(modifier = Modifier.height(8.dp))
 
-            Row(
-                modifier = Modifier.fillMaxWidth(),
-                horizontalArrangement = Arrangement.SpaceBetween
-            ) {
-                Column(
-                    modifier = Modifier
-                        .weight(1f)
-                        .fillMaxWidth(1f),
-//                    verticalArrangement = Arrangement.Center,
-//                    horizontalAlignment = Alignment.CenterHorizontally
-                ) {
-                    Text(
-                        text = provider.location,
-                        style = MaterialTheme.typography.bodyMedium,
-                        textAlign = TextAlign.Center
-                    )
+            Text(
+                text = provider.location,
+                style = MaterialTheme.typography.bodyMedium,
+                textAlign = TextAlign.Center
+            )
 
-                    Spacer(modifier = Modifier.height(8.dp))
+            Spacer(modifier = Modifier.height(8.dp))
 
-                    Text(
-                        text = "Available slots: ${provider.availableSlots}/${provider.totalSlots}",
-                        style = MaterialTheme.typography.bodySmall,
-                        textAlign = TextAlign.Center
-                    )
+            Text(
+                text = "Available slots: ${provider.availableSlots}/${provider.totalSlots}",
+                style = MaterialTheme.typography.bodySmall,
+                textAlign = TextAlign.Center
+            )
 
-                    Spacer(modifier = Modifier.height(8.dp))
+            Spacer(modifier = Modifier.height(8.dp))
 
-                    Text(
-                        text = "Hourly rate:$${provider.hourlyRate}",
-                        style = MaterialTheme.typography.bodySmall,
-                        textAlign = TextAlign.Center
-                    )
-                }
-
-                Image(
-                    painter = painterResource(id = provider.imageUrl),
-                    contentDescription = null,
-                    modifier = Modifier
-                        .fillMaxWidth(0.5f)
-                        .height(120.dp)
-                        .clip(RoundedCornerShape(16.dp)),
-                    contentScale = androidx.compose.ui.layout.ContentScale.Crop
-                )
-
-            }
+            Text(
+                text = "Hourly rate:$ ${provider.hourlyRate}",
+                style = MaterialTheme.typography.bodySmall,
+                textAlign = TextAlign.Center
+            )
         }
     }
 }
