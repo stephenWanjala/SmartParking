25df59d537a7032915a31498d692ee9dab6b0e62
wanjala stephen <74505448+stephenWanjala@users.noreply.github.com>
2024-04-22 23:04:29 +0300

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -41,12 +41,12 @@ android {
             buildConfigField(
                 "String",
                 "CONSUMER_KEY",
-                System.getenv("CONSUMER_KEY") ?: localProperties?.getProperty("CONSUMER_KEY")!!
+                "\"${System.getenv("CONSUMER_KEY")}|"" ?: localProperties?.getProperty("CONSUMER_KEY")!!
             )
             buildConfigField(
                 "String",
                 "CONSUMER_SECRET",
-                System.getenv("CONSUMER_SECRET")
+                "\"${System.getenv("CONSUMER_SECRET")}\""
                     ?: localProperties?.getProperty("CONSUMER_SECRET")!!
             )
         }
@@ -54,13 +54,13 @@ android {
             buildConfigField(
                 "String",
                 "CONSUMER_SECRET",
-                System.getenv("CONSUMER_SECRET")
+                "\"${System.getenv("CONSUMER_SECRET")}\""
                     ?: localProperties?.getProperty("CONSUMER_SECRET")!!
             )
             buildConfigField(
                 "String",
                 "CONSUMER_KEY",
-                System.getenv("CONSUMER_KEY") ?: localProperties?.getProperty("CONSUMER_KEY")!!
+                "\"${System.getenv("CONSUMER_KEY")}\"" ?: localProperties?.getProperty("CONSUMER_KEY")!!
             )
             isMinifyEnabled = false
             proguardFiles(
